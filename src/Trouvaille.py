98ac0fe88489d6495413dcb78a44fc971151c24d
b21827167 <b21827167@cs.hacettepe.edu.tr>
2021-05-22 18:34:15 +0300

    Neural network model implemented

--- src/Trouvaille.py
+++ src/Trouvaille.py
@@ -44,9 +44,72 @@ class Trouvaille:
                     writer = csv.writer(file)
                     writer.writerow(to_append.split())
 
+    def preprocessing_dataset(self):
+        data = pd.read_csv('data.csv')
+        data.head()
+
+        # Dropping unneccesary columns
+        data = data.drop(['filename'], axis=1)
+        data.head()
+
+        genre_list = data.iloc[:, -1]
+        encoder = LabelEncoder()
+        y = encoder.fit_transform(genre_list)
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
+
+        self.X, self.y = unison_shuffled_copies(X, y)
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
+
+    def create_model(self):
+        self.model.add(layers.Dense(256, activation='relu', input_shape=(self.X_train.shape[1],)))
+        self.model.add(layers.Dense(128, activation='relu'))
+        self.model.add(layers.Dense(64, activation='relu'))
+        self.model.add(layers.Dense(10, activation='softmax'))
+        self.model.compile(optimizer='adam',
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['accuracy'])
+
+    def training_model(self):
+        self.model.fit(self.X_train,
+                       self.y_train,
+                       epochs=20,
+                       batch_size=128)
+
+        test_loss, test_acc = self.model.evaluate(self.X_test, self.y_test)
+        print('test_acc: ', test_acc)
+
+        # serialize model to JSON
+        model_json = self.model.to_json()
+        with open("model.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        self.model.save_weights("default_model.h5")
+
+    def predict(self):
+        predictions = self.model.predict(self.X_test)
+        high = np.argmax(predictions[0])
+        print(high)
+
     def run(self):
         print("create_dataset()")
         self.create_dataset()
+
+        print("preprocessing_dataset()")
+        self.preprocessing_dataset()
+
+        print("create_model()")
+        self.create_model()
+
+        print("training_model()")
+        self.training_model()
+
+        print("predict()")
+        self.predict()
+
+
 
 
 def prep_header():
@@ -55,3 +118,8 @@ def prep_header():
         header += f' mfcc{i}'
     header += ' label'
     return header.split()
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
